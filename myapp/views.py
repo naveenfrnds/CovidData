f60b7f33d96b3773665b7c88a6354a97addccdb8
Naveen Nani <naveen@Naveens-MacBook-Pro.local>
2020-03-30 00:56:32 +0530

    statewise data

--- myapp/views.py
+++ myapp/views.py
@@ -377,14 +377,13 @@ def loadstatedata():
     # search = request.POST.get('search')
 
     chrome_options = Options()
-    # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1024x1400")
 
-    driver = webdriver.Chrome(chrome_options=chrome_options,
-                              executable_path="/Users/naveen/Desktop/codedd/myapplist/static/myapp/chromedriver")
+    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=os.environ.get('CHROMEDRIVER_PATH'))
 
     driver.get("https://www.covid19india.org/")
     # assert "GitHub".lower() in driver.title.lower()
